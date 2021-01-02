61d3c8f43b4e6d278f8fb149e3e638dc5f2a082b
Dmitriy <daedaliusx@gmail.com>
2021-01-02 12:26:20 +0300

    installed ts installed webpack

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Application = () => {
+  return <h1>Hello!</h1>;
+};
+
+ReactDOM.render(<Application />, window.document.querySelector("#app"));
